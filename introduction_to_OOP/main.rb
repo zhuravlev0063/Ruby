41af34aa8d4ee4702653a60dd31fb9cc67eff5ef
zhuravlev0063 <zhuravlev0063@gmail.com>
2024-10-25 23:26:14 +0300

    LR2.T1.8 Created methods for checking validity other fields

--- introduction_to_OOP/main.rb
+++ introduction_to_OOP/main.rb
@@ -2,15 +2,15 @@ require_relative 'student'
 
 begin
   # Создание объектов с корректными номерами
-  student1 = Student.new("Ivanov", "Ivan", "Ivanovich", id: 1, phone_number: "+7 (123) 456-78-90", telegram: "@ivanov", email: "ivanov@example.com", git: "github.com/ivanov")
-  student2 = Student.new("Zhuravlev", "Daniil", "Dmitrievich", id: 2, email: "petrov@example.com")
+  student1 = Student.new("Ivanov", "Ivan", "Ivanovich", id: 1, phone_number: "+7 (123) 456-78-90", telegram: "@ivanov", email: "ivanov@example.com", git: "https:github.com/ivanov")
+  student2 = Student.new("Zhuravlev", "Daniil", "Dmitrievich", id: 2, email: "danil@example.com")
 
   # Вывод информации о студентах
   puts student1
   puts student2
 
   # Попытка создания объекта с некорректным номером телефона
-  student3 = Student.new("Osipov", "Vasiliy", "Romanovich", id: 3, phone_number: "123ABC")
+  student3 = Student.new("Osipov", "Vasiliy", "Romanovich", id: 3, phone_number: "+7 123 456 78 90", telegram: "vasiliy", email: "vasiliy.com")
 rescue ArgumentError => e
   puts e.message
 end
--- introduction_to_OOP/student.rb
+++ introduction_to_OOP/student.rb
@@ -1,35 +1,70 @@
 class Student
-  attr_accessor :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
+  # Автоматическое создание геттеров для всех полей
+  attr_accessor :id, :telegram
+  attr_reader :surname, :firstname, :lastname, :telegram, :email, :git, :phone_number
 
-  # Конструктор с проверкой номера телефона
+  # Конструктор
   def initialize(surname, firstname, lastname, id: nil, phone_number: nil, telegram: nil, email: nil, git: nil)
-    @surname = surname
-    @firstname = firstname
-    @lastname = lastname
+    self.surname = surname
+    self.firstname = firstname
+    self.lastname = lastname
     @id = id
-
-    # Проверка номера телефона через метод класса
-    if Student.is_phone_number_valid?(phone_number)
-      @phone_number = phone_number
-    else
-      raise ArgumentError.new("Неверный номер телефона для пользователя #{@id} #{@surname} #{@firstname} #{@lastname}")
-    end
-
-    @telegram = telegram
-    @email = email
-    @git = git
+    self.phone_number = phone_number
+    self.telegram = telegram
+    self.email = email
+    self.git = git
   end
 
-  # Метод класса для проверки номера телефона
-  def self.is_phone_number_valid?(checked_phone_number)
-    phone_number_reg = /^\+?\d\s?\(?\s*\d{3}\s*\)?\s?\d{3}\-{0,1}\d{2}\-{0,1}\d{2}\s*$/
-    return checked_phone_number =~ phone_number_reg
+  # Метод для общей проверки валидности
+  def self.validate_field(value, regex, error_message)
+    return value if value.nil? || value =~ regex
+
+    raise ArgumentError.new(error_message)
+  end
+
+  # Валидация фамилии
+  def surname=(surname)
+    @surname = Student.validate_field(surname, /^[A-Za-zА-яа-я]+$/, "Неверная фамилия студента: #{@id}")
+  end
+
+  # Валидация имени
+  def firstname=(firstname)
+    @firstname = Student.validate_field(firstname, /^[A-Za-zА-яа-я]+$/, "Неверное имя студента: #{@id} #{@surname}")
+  end
+
+  # Валидация отчества
+  def lastname=(lastname)
+    @lastname = Student.validate_field(lastname, /^[A-Za-zА-яа-я]+$/, "Неверное отчество студента: #{@id} #{@surname} #{@firstname}")
+  end
+
+  # Валидация номера телефона
+  def phone_number=(phone_number)
+    phone_number_reg = /^\+?\d{1,3}\s?\(?\d{3}\)?\s?\d{3}-?\d{2}-?\d{2}$/
+    @phone_number = Student.validate_field(phone_number, phone_number_reg, "Неверный номер телефона для студента: #{@id} #{@surname} #{@lastname} #{@firstname}")
+  end
+
+  # Валидация email
+  def email=(email)
+    email_reg = /^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/i
+    @email = Student.validate_field(email, email_reg, "Неверный адрес электронной почты: #{@id} #{@surname} #{@lastname} #{@firstname}")
+  end
+
+  # Валидация git
+  def git=(git)
+    git_reg = /^https:\/\/github\.com\/[A-Za-z0-9._-]+\/?$/
+    @git = Student.validate_field(git, git_reg, "Неверная ссылка Git: #{@id} #{@surname} #{@lastname} #{@firstname}")
+  end
+
+  # Валидация Telegram handle
+  def telegram=(telegram)
+    telegram_reg = /^@[a-zA-Z0-9_]{5,}$/
+    @telegram = Student.validate_field(telegram, telegram_reg, "Неверный Telegram handle для студента: #{@id} #{@surname} #{@lastname} #{@firstname}")
   end
 
   # Метод для вывода информации об объекте
   def to_s
-    info = "#{@id} ФИО: #{@surname} #{@firstname} #{@lastname}.\n"
-    contact_info = "Данные для связи:\n"
+    info = "#{@id} #{@surname} #{@firstname} #{@lastname}.\nДанные для связи:\n"
+    contact_info = ""
     contact_info += "Номер телефона: #{@phone_number}\n" if @phone_number
     contact_info += "Телеграм: #{@telegram}\n" if @telegram
     contact_info += "Email: #{@email}\n" if @email

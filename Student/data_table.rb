cf72f5b27448a746da794e2af87e69dfe3e566c3
zhuravlev0063 <zhuravlev0063@gmail.com>
2025-01-22 19:12:08 +0300

    LR4.T1 created class Data_table

---
+++ Student/data_table.rb
@@ -0,0 +1,15 @@
+class Data_table
+  def initialize(data)
+    @data = data
+  end
+
+  private 
+
+  def data=(data)
+    if data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
+      @data = data
+    else
+      raise TypeError, "Data must be a two-dimensional array"
+    end
+  end
+end

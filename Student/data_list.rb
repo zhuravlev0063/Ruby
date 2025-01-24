7ca5294864b51357385e126931c6989c6f85072c
zhuravlev0063 <zhuravlev0063@gmail.com>
2025-01-25 02:05:44 +0300

    LR4.T1.7 Used template pattern

--- Student/data_list.rb
+++ Student/data_list.rb
@@ -1,6 +1,8 @@
 class Data_list
   def initialize(list)
     self.list = list
+    self.selected_elems = []
+    self.index = 0
   end
   
   def select(index)
@@ -11,13 +13,27 @@ class Data_list
     raise "Index out of range" if index < 0 || index >= list.length
     self.selected_indexes << index
   end
-
-  def get_names
-  end
   
+  def get_names
+    raise NotImplementedError, "Must be implemented!"
+  end
+
   def get_data
+    result_data = []
+    self.selected_indexes.each do |index|
+      data = self.list[index]
+      new_row = [self.index] + row_from_attrs(data)
+      result_data << new_row 
+      self.index++
+    end
+    result_data
   end
 
   private
-  attr_accessor :list,  :selected_elems
+  attr_accessor :list,  :selected_elems :index
+
+  def row_from_attrs(data) 
+    raise NotImplementedError, "Must be implemented!"
+  end
+
 end
--- Student/‎data_list_student_short.rb
+++ Student/‎data_list_student_short.rb
@@ -1,19 +1,16 @@
 require_relative 'data_list.rb'
 require_relative 'data_table.rb'
+
 class Data_list_student_short < Data_list
   
-  def get_data
-    result_data = []
-    self.selected_indexes.each do |index|
-      data = self.list[index]
-      new_row = [self.index, data.fullname, data.git, data.contact] 
-        result_data << new_row 
-    end
-    result_data
-  end
+
   def get_names
     ['fullname', 'git', 'contact']
   end
+
   private
-  attr_accessor :index
+
+  def row_from_attrs(data)
+    return [data.initials, data.git, data.contact]
+  end
 end

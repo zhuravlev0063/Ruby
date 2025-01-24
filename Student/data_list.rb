819ea5f20c28e8cab5f57f7fa1f250a3b45137cf
zhuravlev0063 <zhuravlev0063@gmail.com>
2025-01-24 17:55:03 +0300

    LR4.T1.4a created method get_select and 2 abstract methods

--- Student/data_list.rb
+++ Student/data_list.rb
@@ -6,7 +6,19 @@ class Data_list
   def select(index)
     self.selected_elems = list[index]
   end
+  
+  def get_select(index)
+    self.selected_elems = list[index]
+    raise "Index out of range" if index < 0 || index >= list.length
+    self.selected_indexes << index
+  end
 
+  def get_names
+  end
+  
+  def get_data
+  end
+  
   private
   attr_accessor :list,  :selected_elems
 end
